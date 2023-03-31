18f518277e5e902d6943434ca522493c1ea154a6
xuwen <xuwen2@codemao.cn>
2023-03-31 16:32:44 +0800

    first commit

---
+++ src/view/pages/setting.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="setting">
+    <el-container>
+      <el-header>
+        <transition name="el-zoom-in-top">
+          <div v-show="show" class="transition-box">
+            <h3>M O O N B O X</h3>
+          </div>
+        </transition>
+      </el-header>
+      <el-main>
+        <transition name="el-zoom-in-top">
+          <div v-show="show" class="transition-box">
+            <span>
+              现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API。
+            </span>
+          </div>
+        </transition>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      show: false,
+    };
+  },
+  mounted() {
+    this.show = true;
+  },
+};
+</script>
+
+<style>
+  .transition-box {
+    text-align: center;
+    margin-top: 5%;
+    color: #F2F6FC;
+    font-size: 30px;
+  }
+  h1 {
+    color: aliceblue;
+    font-size: 80px;
+  }
+  a {
+    text-decoration: none;
+    color: #67C23A;
+  }
+</style>
